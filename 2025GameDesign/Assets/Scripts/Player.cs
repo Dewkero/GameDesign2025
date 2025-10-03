fad63eca9d935375e9072d66ce3c0f7835a144e7
Dewkero <wilm7624@gmail.com>
2025-10-02 22:11:34 -0500

    Start of Hide Code
    Hide bool works, but now player cant get damaged

--- 2025GameDesign/Assets/Scripts/Player.cs
+++ 2025GameDesign/Assets/Scripts/Player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -12,6 +14,10 @@ public class Player : MonoBehaviour
 
     public bool flippedleft;
     public bool facingRight;
+
+    // hiding variables
+    public bool canHide = false;
+    public bool hiding = false;
 
     // Update is called once per frame
     void Update()
@@ -34,8 +40,53 @@ public class Player : MonoBehaviour
 
     }
 
+
+    //hiding
+    private void LateUpdate()
+    {
+        if(canHide && Input.GetKey(KeyCode.W))
+        {
+            Physics2D.IgnoreLayerCollision(6, 7, true);
+            hiding = true; 
+        }
+        else
+        {
+            Physics2D.IgnoreLayerCollision(6, 7, false);
+            hiding = false;
+        }
+
+    }
+
     private void FixedUpdate()
     {
         playerRB.velocity = new Vector2(input * speed, playerRB.velocity.y);
+
+
+        // hiding
+        if (!hiding)
+        {
+            playerRB.velocity = new Vector2(input * speed, playerRB.velocity.y);
+        }
+        else
+        {
+            playerRB.velocity = Vector2.zero;
+        }
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("HidingSpot"))
+        {
+            canHide = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("HidingSpot"))
+        {
+            canHide = false;
+        }
+    }
+
 }
