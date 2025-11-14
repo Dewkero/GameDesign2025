6328e3f49adfa5c39682db2ca3a98b7a23e38198
Dewkero <wilm7624@gmail.com>
2025-11-13 19:08:43 -0600

    Added player health UI

--- 2025GameDesign/Assets/Scripts/PlayerHealth.cs
+++ 2025GameDesign/Assets/Scripts/PlayerHealth.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 10;
     public int currentHealth;
+
+    public Image healthBarFill;
+    public GameObject healthBar;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +20,14 @@ public class PlayerHealth : MonoBehaviour
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+
+        healthBarFill.fillAmount = currentHealth / maxHealth;
+
         if (currentHealth <= 0)
         {
             Destroy(gameObject);
         }
+
+        healthBar.SetActive(true);
     }
 }
