1b22e5cc80e2f292b4e17926fce68f3fe940c94c
Dewkero <wilm7624@gmail.com>
2025-11-20 20:49:25 -0600

    Added Round Timer

---
+++ 2025GameDesign/Assets/Scripts/Timer.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    private float timerDuration = 3f * 60f;
+    private float timer;
+
+    [SerializeField]
+    private TextMeshProUGUI firstMinute;
+    [SerializeField]
+    private TextMeshProUGUI secondMinute;
+    [SerializeField]
+    private TextMeshProUGUI seperator;
+    [SerializeField]
+    private TextMeshProUGUI firstSecond;
+    [SerializeField]
+    private TextMeshProUGUI secondSecond;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetTimer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 0)
+        { 
+        timer -= Time.deltaTime;
+            UpdateTimerDisplay(timer);
+        }
+    }
+
+    private void ResetTimer()
+    {
+        timer = timerDuration;
+    }
+
+    private void UpdateTimerDisplay(float time)
+    {
+        float minutes = Mathf.FloorToInt(time / 60);
+        float seconds = Mathf.FloorToInt(time % 60);
+
+        string currenTime = string.Format("{0:00}{1:00}", minutes, seconds);
+        firstMinute.text = currenTime[0].ToString();
+        secondMinute.text = currenTime[1].ToString();
+        firstSecond.text = currenTime[2].ToString();
+        secondSecond.text = currenTime[3].ToString();
+    }
+
+}
